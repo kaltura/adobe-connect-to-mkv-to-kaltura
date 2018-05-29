96330b135d80def56d493b4d9c79f04aef078473
Jess Portnoy <jess.portnoy@kaltura.com>
2018-05-29 16:56:29 +0100

    .

--- ac_wrapper.sh
+++ ac_wrapper.sh
@@ -4,7 +4,7 @@ if [ $# -lt 1 ];then
         exit 1
 fi
 
-for UTIL in pidof xvfb-run xvfb-run-safe curl unzip;do
+for UTIL in pidof xvfb-run xvfb-run-safe curl unzip dos2unix;do
         if [ ! -x "`which $UTIL 2>/dev/null`" ];then
                 echo "Need to install $UTIL."
                 exit 2
@@ -16,6 +16,7 @@ ASSET_LIST_FILE=$1
 if [ -z "$MAX_CONCUR_PROCS" ];then
     MAX_CONCUR_PROCS=7
 fi
+dos2unix $ASSET_LIST_FILE
 while IFS=, read -r SCO_ID CATEGORY_NAME MEETING_NAME MEETING_ID;do
 	set -o nounset
         CATEGORY_NAME=`echo $CATEGORY_NAME|sed 's@"@@g'`
