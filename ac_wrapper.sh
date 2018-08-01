584b0239e2692270bfd3b8d3830263c8bb755cef
Jess Portnoy <jess.portnoy@kaltura.com>
2018-08-01 10:54:23 -0400

    - Make scene detection less sensitive [with `select='gt(scene,0.03)'`, it sometimes detected one of the "Connecting..." frames as the first scene] - ac_wrapper.sh: use a designated file descriptor for the main [outter] while loop to prevent stdin hijacking

--- ac_wrapper.sh
+++ ac_wrapper.sh
@@ -19,12 +19,12 @@ fi
 if [ -x "`which dos2unix 2>/dev/null`" ];then
     dos2unix $ASSET_LIST_FILE
 fi
-while IFS=, read -r SCO_ID CATEGORY_NAME MEETING_NAME MEETING_ID;do
+while IFS=, read -r SCO_ID CATEGORY_NAME MEETING_NAME MEETING_ID <&3 ;do
 	set -o nounset
         CUR_XVFB=`pidof Xvfb |wc -w`
         while [ ! $CUR_XVFB -lt $MAX_CONCUR_PROCS ];do
                 echo "Have $CUR_XVFB running so I'll take a short nap..."
-                sleep 60
+		sleep 60
                 CUR_XVFB=`pidof Xvfb |wc -w`
         done
         CATEGORY_NAME=`echo $CATEGORY_NAME|sed 's^"^^g'`
@@ -32,5 +32,5 @@ while IFS=, read -r SCO_ID CATEGORY_NAME MEETING_NAME MEETING_ID;do
         export CATEGORY_NAME MEETING_NAME MEETING_ID
         nohup sh -c "xvfb-run-safe -s \"-auth /tmp/xvfb.auth -ac -screen 0 1280x720x24\" $BASEDIR/ac_new.rb " > /tmp/ac_$MEETING_ID.log 2>&1 &
 	sleep 2
-done < $ASSET_LIST_FILE 
+done 3< $ASSET_LIST_FILE 
 
